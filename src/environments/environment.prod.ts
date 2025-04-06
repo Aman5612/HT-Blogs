250f70bb663e6291fca97ab95f8f7ef9f6d4b52f
allmattr <tech.allmattr@gmail.com>
2025-04-07 03:18:22 +0530

    chore: minor changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
-  fallbackApiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://holidayer.in:3009/api',
+  fallbackApiUrl: 'https://holidayer.in:3009/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api',
-  fallbackApiUrl: 'http://localhost:3000/api', // Fallback API URL
+  production: true,
+  apiUrl: 'https://holidayer.in:3009/api',
+  fallbackApiUrl: 'https://holidayer.in:3009/api',
 };
